2e4e4b00992edce319bfa833c8bf403ac6664f40
harry <harryleiyyds@gmail.com>
2025-05-27 15:20:27 +0800

    feat: 版本迁移和初始化 service 和 web

---
+++ bytelogs-web/src/main/java/top/harrylei/bytelogsweb/BytelogsWebApplication.java
@@ -0,0 +1,13 @@
+package top.harrylei.bytelogsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytelogsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytelogsWebApplication.class, args);
+    }
+
+}
