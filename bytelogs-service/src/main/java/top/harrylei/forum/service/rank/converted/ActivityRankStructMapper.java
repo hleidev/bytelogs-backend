5abfd2b7bafadb6b08d7c363aa854532e0cf998e
harry <harryleiyyds@gmail.com>
2025-08-01 17:28:26 +0800

    refactor: 重构活跃度排行榜API模型结构
    - 删除旧的ActivityRankQueryParam请求参数类
    - 新增ActivityRankDTO数据传输对象，包含用户基本信息和排名数据
    - 新增ActivityRankListVO响应对象，支持排行榜列表和个人排名返回
    - 新增ActivityRankStructMapper映射器，实现DTO到VO的转换
    - 优化API层数据结构设计，提升代码可维护性

---
+++ bytelogs-service/src/main/java/top/harrylei/forum/service/rank/converted/ActivityRankStructMapper.java
@@ -0,0 +1,12 @@
+package top.harrylei.forum.service.rank.converted;
+
+import org.mapstruct.Mapper;
+
+/**
+ * 排行榜结构映射器
+ *
+ * @author harry
+ */
+@Mapper(componentModel = "spring")
+public interface RankStructMapper {
+}
